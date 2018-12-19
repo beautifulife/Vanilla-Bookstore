ba0d6cf3ee4ac4e6a2c9e166e8219c4ab9a69126
Beautifulife <davidkim1018@gmail.com>
2018-12-19 19:03:44 +0900

    Make search logic and finish making api connection

--- app/index.js
+++ app/index.js
@@ -13,27 +13,87 @@ import grandBabyTemplate from 'grandBaby.ejs';
 // Import Gorilla Module
 import Gorilla from '../Gorilla';
 
-/* AJAX TEST~ */
+const $header = document.getElementsByTagName('header')[0];
+const $mainContent = document.getElementById('main_content');
+const $inputBox = document.getElementById('search_box');
+const $searchButton = document.getElementById('search_button');
+const $getMoreButton = document.getElementById('get_more_button');
+const $toUpwardButton = document.getElementById('to_upward');
+let $nowSearch = [];
+let $beforeScroll = 0;
 
-const getApiButton = document.getElementById('test');
-let keyWord = encodeURI('자바스크립트');
+$inputBox.addEventListener('keypress', getText);
+$searchButton.addEventListener('click', startSearch);
+$getMoreButton.addEventListener('click', getMoreData);
+$toUpwardButton.addEventListener('click', moveToUpward);
+document.addEventListener('scroll', controlByScroll);
 
-getApiButton.addEventListener('click', getDataFromApi);
+function getText(ev) {
+  if (ev.currentTarget.value && ev.code === 'Enter') {
+    removeBeforeSearch();
+    ev.currentTarget.disabled = true;
+    $nowSearch = [ev.currentTarget.value, 21];
+    getDataFromApi(ev.currentTarget.value, 1);
+  }
 
-function getDataFromApi() {
-  console.log('pick me up!');
+  function removeBeforeSearch() {
+    if ($mainContent.children[0].children.length) {
+      const div = document.createElement('div');
 
+      $mainContent.children[0].remove();
+      div.id = 'root';
+      $mainContent.appendChild(div);
+    }
+  }
+}
+
+function startSearch(ev) {
+  if ($inputBox.value) {
+    getDataFromApi($inputBox.value);
+  }
+}
+
+function getMoreData(ev) {
+  getDataFromApi($nowSearch[0], $nowSearch[1]);
+  $nowSearch[1] += 20;
+}
+
+function moveToUpward(ev) {
+  document.body.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+}
+
+function controlByScroll(ev) {
+  const bodyYOffset = window.pageYOffset;
+
+  if (bodyYOffset !== 0) {
+    $toUpwardButton.classList.remove('hidden');
+  } else {
+    $toUpwardButton.classList.add('hidden');
+  }
+
+  if (bodyYOffset >= 300 && bodyYOffset - $beforeScroll >= 0) {
+    $header.classList.add('scroll_bottom');
+    $beforeScroll = bodyYOffset;
+  } else if (bodyYOffset - $beforeScroll < 0) {
+    $header.classList.remove('scroll_bottom');
+    $beforeScroll = bodyYOffset;
+  }
+}
+
+function getDataFromApi(inputText, startNum) {
+  const keyWord = encodeURI(inputText);
   const httpRequest = new XMLHttpRequest();
-  const url = 'http://localhost:3000/v1/search/book?query=' + keyWord + '&display=2&sort=count';
+  const url = `http://localhost:3000/v1/search/book?query=${keyWord}&start=${startNum}&display=20&sort=count`;
 
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === httpRequest.DONE) {
-      if (httpRequest.status === 200 || httpRequest.status === 201) {
+      if (httpRequest.status === 200) {
         const bookMasterData = JSON.parse(httpRequest.response);
         
         console.log(bookMasterData);
         
         for (let i = 0; i < bookMasterData.items.length; i++) {
+          console.log('now i send data');
           compressUrl(bookMasterData.items[i]);
         }
       } else {
@@ -52,13 +112,13 @@ function compressUrl(bookData) {
 
   httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === httpRequest.DONE) {
-      if (httpRequest.status === 200 || httpRequest.status === 201) {
+      if (httpRequest.status === 200) {
         const responsedUrl = JSON.parse(httpRequest.response);
 
         bookData.link = responsedUrl.result.url;
-        console.log(bookData.link);
+        console.log(bookData.link, 'this is short link');
 
-        makeComponent(bookData);
+        makeComponent(cleansData(bookData));
       } else {
         console.log(httpRequest.response);
       }
@@ -69,19 +129,45 @@ function compressUrl(bookData) {
   httpRequest.send();
 }
 
-function makeComponent(bookData) {
-  let indexofBracket = bookData.title.indexOf('(');
-  console.log(typeof bookData.title);
-  let title;
-  let subTitle
+function cleansData(bookData) {
+  const indexofBracket = bookData.title.indexOf('(');
 
-  if (indexofBracket) {
-    title = bookData.title.substring(0, indexofBracket);
-    subTitle = bookData.title.substring(indexofBracket + 1, bookData.title.length - 1);
+  if (indexofBracket !== -1) {
+    bookData.subTitle = bookData.title.substring(indexofBracket + 1, bookData.title.length - 1);
+    bookData.title = bookData.title.substring(0, indexofBracket);
   }
 
+  if (!bookData.discount) {
+    bookData.discount = addComma(bookData.price);
+    bookData.price = null;
+  } else {
+    bookData.price = addComma(bookData.price);
+    bookData.discount = addComma(bookData.discount);
+  }
+
+  if (!bookData.image) {
+    bookData.image = '/assets/images/replace_img.jpg';
+  }
+
+  return bookData;
+
+  function addComma(priceString) {
+    const priceArray = priceString.split('');
+    let index = -3;
+
+    while (priceArray.length + index > 0) {
+      priceArray.splice(index, 0, ',');
+      index -= 4;
+    }
+
+    return priceArray.join('');
+  }
+}
+
+function makeComponent(bookData) {
   const grandBaby = new Gorilla.Component(grandBabyTemplate, {
-    content: null,
+    sales: bookData.price,
+    discount: bookData.discount,
   });
 
   grandBaby.hello = function () {
@@ -90,7 +176,10 @@ function makeComponent(bookData) {
 
   const baby = new Gorilla.Component(babyTemplate, {
     name: bookData.author,
+    publisher: bookData.publisher,
+    date: bookData.pubdate,
     description: bookData.description,
+    url: bookData.link,
   }, {
     grandBaby,
   });
@@ -101,9 +190,8 @@ function makeComponent(bookData) {
 
   const app = new Gorilla.Component(appTemplate, {
     image: bookData.image,
-    title,
-    subTitle,
-    price: bookData.price,
+    title: bookData.title,
+    subTitle: bookData.subTitle,
   }, {
     baby,
   });
@@ -121,8 +209,10 @@ function makeComponent(bookData) {
 
   Gorilla.renderToDOM(
     app,
-    document.querySelector('#root')
+    document.querySelector('#main_content').firstElementChild,
   );
+
+  $inputBox.disabled = false;
 }
 
 /* AJAX TEST~ */
