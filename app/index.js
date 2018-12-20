694100d23e339e79936a59cb9a83d9b9ac4a5201
Beautifulife <davidkim1018@gmail.com>
2018-12-20 19:15:20 +0900

    Rebuild code, using gorilla.js
    Make gorilla component. make array data from api.
    
    [Ticket: X]

--- app/index.js
+++ app/index.js
@@ -7,78 +7,99 @@ import 'styles/index.less';
 
 // Importing component templates
 import appTemplate from 'app.ejs';
-import babyTemplate from 'baby.ejs';
-import grandBabyTemplate from 'grandBaby.ejs';
+import contentsTemplate from 'contents.ejs';
+import contentContainerTemplate from 'contentContainer.ejs';
 
 // Import Gorilla Module
 import Gorilla from '../Gorilla';
 
-const $header = document.getElementsByTagName('header')[0];
-const $mainContent = document.getElementById('main_content');
-const $inputBox = document.getElementById('search_box');
-const $searchButton = document.getElementById('search_button');
-const $getMoreButton = document.getElementById('get_more_button');
-const $toUpwardButton = document.getElementById('to_upward');
-let $nowSearch = [];
+let $nowSearch = 1;
 let $beforeScroll = 0;
+let $count = 0;
+const $bookData = [];
 
-$inputBox.addEventListener('keypress', getText);
-$searchButton.addEventListener('click', startSearch);
-$getMoreButton.addEventListener('click', getMoreData);
-$toUpwardButton.addEventListener('click', moveToUpward);
-document.addEventListener('scroll', controlByScroll);
+const app = new Gorilla.Component(appTemplate, {
+  title: 'VAmazon',
+  subTitle: 'The bookstore by Vanilla_Coding',
+  searchboxPlaceholder: 'subject, author, publisher',
+  contact: 'beautifulife.github.io',
+}, {
+  contents: new Gorilla.Component(contentContainerTemplate),
+});
 
-function getText(ev) {
+window.addEventListener('scroll', controlByScroll);
+
+app.reloadClick = function (ev) {
+  location.reload();
+}
+
+app.handleKeypress = function (ev) {
   if (ev.currentTarget.value && ev.code === 'Enter') {
-    removeBeforeSearch();
+    // removeBeforeSearch();
     ev.currentTarget.disabled = true;
-    $nowSearch = [ev.currentTarget.value, 21];
     getDataFromApi(ev.currentTarget.value, 1);
+    $nowSearch = 21;
   }
 
-  function removeBeforeSearch() {
-    if ($mainContent.children[0].children.length) {
-      const div = document.createElement('div');
+  // function removeBeforeSearch() {
+  //   if ($mainContent.children[0].children.length) {
+  //     const div = document.createElement('div');
 
-      $mainContent.children[0].remove();
-      div.id = 'root';
-      $mainContent.appendChild(div);
-    }
+  //     $mainContent.children[0].remove();
+  //     div.id = 'root';
+  //     $mainContent.appendChild(div);
+  //   }
+  // }
+};
+
+app.handleSearchClick = function (ev) {
+  if (ev.currentTarget.previousElementSibling.value) {
+    getDataFromApi(ev.currentTarget.previousElementSibling.value, $nowSearch);
+    $nowSearch += 20;
   }
-}
+};
 
-function startSearch(ev) {
-  if ($inputBox.value) {
-    getDataFromApi($inputBox.value);
-  }
-}
+app.handleUpwardClick = function (ev) {
+  ev.currentTarget.parentNode.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+};
 
-function getMoreData(ev) {
-  getDataFromApi($nowSearch[0], $nowSearch[1]);
-  $nowSearch[1] += 20;
-}
+// Listening to component life cycle
+app.on('BEFORE_RENDER', () => console.log('app before render'));
+app.on('AFTER_RENDER', () => console.log('app after render'));
 
-function moveToUpward(ev) {
-  $mainContent.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
-}
+//   contentContainer.on('BEFORE_RENDER', () => console.log('cont2 before render'));
+//   contentContainer.on('AFTER_RENDER', () => console.log('cont2 after render'));
+//   contents._view.children.contentContainer2 = contentContainer2;
+//   contents.render();
+
+// Initializing the app into DOM
+Gorilla.renderToDOM(
+  app,
+  document.querySelector('#root'),
+);
+
+const $header = document.getElementsByTagName('header')[0];
+const $toUpwardButton = document.getElementById('to_upward');
 
 function controlByScroll(ev) {
-  const bodyYOffset = window.pageYOffset;
+  const yOffset = ev.currentTarget.pageYOffset;
 
-  if (bodyYOffset !== 0) {
+  if (yOffset !== 0) {
     $toUpwardButton.classList.remove('hidden');
   } else {
     $toUpwardButton.classList.add('hidden');
   }
 
-  if (bodyYOffset >= 300 && bodyYOffset - $beforeScroll >= 0) {
+  if (yOffset >= 300 && yOffset - $beforeScroll >= 0) {
     $header.classList.add('scroll_bottom');
-    $beforeScroll = bodyYOffset;
-  } else if (bodyYOffset - $beforeScroll < 0) {
+    $beforeScroll = yOffset;
+  } else if (yOffset - $beforeScroll < 0) {
     $header.classList.remove('scroll_bottom');
-    $beforeScroll = bodyYOffset;
+    $beforeScroll = yOffset;
   }
 }
+
+//
 
 function getDataFromApi(inputText, startNum) {
   const keyWord = encodeURI(inputText);
@@ -89,9 +110,7 @@ function getDataFromApi(inputText, startNum) {
     if (httpRequest.readyState === httpRequest.DONE) {
       if (httpRequest.status === 200) {
         const bookMasterData = JSON.parse(httpRequest.response);
-        
-        console.log(bookMasterData);
-        
+
         for (let i = 0; i < bookMasterData.items.length; i++) {
           console.log('now i send data');
           compressUrl(bookMasterData.items[i]);
@@ -116,9 +135,14 @@ function compressUrl(bookData) {
         const responsedUrl = JSON.parse(httpRequest.response);
 
         bookData.link = responsedUrl.result.url;
-        console.log(bookData.link, 'this is short link');
+        $bookData.push(cleansData(bookData));
 
-        makeComponent(cleansData(bookData));
+        $count++;
+
+        if ($count === 20) {
+          makeComponent($bookData);
+          $count = 0;
+        }
       } else {
         console.log(httpRequest.response);
       }
@@ -135,6 +159,10 @@ function cleansData(bookData) {
   if (indexofBracket !== -1) {
     bookData.subTitle = bookData.title.substring(indexofBracket + 1, bookData.title.length - 1);
     bookData.title = bookData.title.substring(0, indexofBracket);
+  }
+
+  if (bookData.description.length >= 50) {
+    bookData.description = bookData.description.substring(0, 50);
   }
 
   if (!bookData.discount) {
@@ -165,110 +193,16 @@ function cleansData(bookData) {
 }
 
 function makeComponent(bookData) {
-  const grandBaby = new Gorilla.Component(grandBabyTemplate, {
-    sales: bookData.price,
-    discount: bookData.discount,
+  const contents = new Gorilla.Component(contentsTemplate, {
+    name: 'Baby.',
+    contentContainer: bookData,
   });
 
-  grandBaby.hello = function () {
-    console.log('hello');
-  };
+  app._view.children.contents = contents;
 
-  const baby = new Gorilla.Component(babyTemplate, {
-    name: bookData.author,
-    publisher: bookData.publisher,
-    date: bookData.pubdate,
-    description: bookData.description,
-    url: bookData.link,
-  }, {
-    grandBaby,
-  });
-
-  baby.whatAreYou = function () {
-    console.log('what are you?');
-  };
-
-  const app = new Gorilla.Component(appTemplate, {
-    image: bookData.image,
-    title: bookData.title,
-    subTitle: bookData.subTitle,
-  }, {
-    baby,
-  });
-
-  app.handleMouseover = function () {
-    console.log('mouseover');
-  };
-
-  app.handleClick = function () {
-    console.log('click');
-  };
-
-  app.on('BEFORE_RENDER', () => console.log('app before render'));
-  app.on('AFTER_RENDER', () => console.log('app after render'));
-
-  Gorilla.renderToDOM(
-    app,
-    document.querySelector('#main_content').firstElementChild,
-  );
-
-  $inputBox.disabled = false;
+  app.render();
 }
 
-/* AJAX TEST~ */
-
-// // Creating Gorilla Component Instance with some data
-// const grandBaby = new Gorilla.Component(grandBabyTemplate, {
-//   content: 'What!!!'
-// });
-
-// // Creating Component Instance Method (Frequently used as event handler in template)
-// grandBaby.hello = function () {
-//   console.log('hello');
-// };
-
-// // Creating Gorilla Component Instance with some data and child component
-// const baby = new Gorilla.Component(babyTemplate, {
-//   name: 'Baby..'
-// }, {
-//   grandBaby
-// });
-
-// baby.whatAreYou = function () {
-//   console.log('what are you?');
-// };
-
-// const app = new Gorilla.Component(appTemplate, {
-//   title: '바닐라코딩'
-// }, {
-//   baby
-// });
-
-// app.handleMouseover = function () {
-//   console.log('mouseover');
-// };
-
-// app.handleClick = function () {
-//   console.log('click');
-// };
-
-// // Listening to component life cycle
-// app.on('BEFORE_RENDER', () => console.log('app before render'));
-// app.on('AFTER_RENDER', () => console.log('app after render'));
-
-// Updating component data model
-// setTimeout(() => {
-//   app.title = '빠닐라코띵';
-//   baby.name = 'Qkqkqkqk';
-//   grandBaby.content = 'GGGGG';
-// }, 2000);
-
-// Initializing the app into DOM
-// Gorilla.renderToDOM(
-//   app,
-//   document.querySelector('#root')
-// );
-
 /* DO NOT REMOVE */
-// module.hot.accept();
+module.hot.accept();
 /* DO NOT REMOVE */
